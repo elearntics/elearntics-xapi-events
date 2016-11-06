e3f2356cb11980a2151d8e780e313344f87b0ec5
elenatorro <elenatorro@gmail.com>
2016-11-06 18:02:11 +0100

    ADDED: submodule xAPI-Validator-JS from adlnet. ADDED: default statement initialization

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,83 @@
-var gulp = require('gulp')
-var jshint = require('gulp-jshint')
-var watch = require('gulp-watch')
-var scriptsToWatch = ['./lib/*.js', './test/*.js']
+'use strict';
 
-gulp.task('lint', function () {
-  return gulp.src(scriptsToWatch)
-    .pipe(watch(scriptsToWatch))
-    .pipe(jshint())
-    .pipe(jshint.reporter('fail'))
-})
+/* Libraries */
+let
+  browserify  = require('browserify'),
+  buffer      = require('vinyl-buffer'),
+  connect     = require('gulp-connect'),
+  gulp        = require('gulp'),
+  livereload  = require('gulp-livereload'),
+  source      = require('vinyl-source-stream'),
+  sourcemaps  = require('gulp-sourcemaps'),
+  uglify      = require('gulp-uglify')
+;
 
-gulp.task('default', ['lint'])
+/* Config */
+const
+  BABEL_CONFIG = Object.freeze({
+    presets: ['es2015']
+  }),
+  BROWSERIFY_TRANSFORM = 'babelify',
+  BROWSERIFY_CONFIG = Object.freeze({
+    debug: true,
+    json: true
+  }),
+  DIST_FILENAME      = 'xapi-events.js',
+  DIST_PATH          = 'dist',
+  DIST_TEST_FILENAME = 'test.xapi-events.js',
+  DIST_TEST_PATH     = 'dist-test',
+  MAPS_PATH          = './maps',
+  SRC_FILE           = 'lib/xAPI-events.js',
+  SRC_TEST_FILE      = 'test/test.main.js',
+  WATCH_FILES        = ['lib/*.js', 'lib/**/*.js', 'test/*.js', 'test/**/*.js']
+;
+
+/* Task Config */
+
+const
+  BUILD_TASK       = 'build',
+  BUILD_TEST_TASK  = 'build-test',
+  CONNECT_TASK     = 'connect',
+  DEFAULT_TASK     = 'default',
+  DEFAULT_TASKS    = [BUILD_TASK, BUILD_TEST_TASK],
+  WATCH_TASK       = 'watch',
+  WATCH_TASKS      = [BUILD_TASK, BUILD_TEST_TASK]
+;
+
+gulp.task(BUILD_TASK, () => {
+  return browserify(SRC_FILE, BROWSERIFY_CONFIG)
+    .transform(BROWSERIFY_TRANSFORM, BABEL_CONFIG)
+    .bundle()
+    .pipe(source(DIST_FILENAME))
+    .pipe(buffer())
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write(MAPS_PATH))
+    .pipe(gulp.dest(DIST_PATH))
+    .pipe(livereload());
+});
+
+gulp.task(BUILD_TEST_TASK, () => {
+  return browserify(SRC_TEST_FILE, BROWSERIFY_CONFIG)
+    .transform(BROWSERIFY_TRANSFORM, BABEL_CONFIG)
+    .bundle()
+    .pipe(source(DIST_TEST_FILENAME))
+    .pipe(buffer())
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write(MAPS_PATH))
+    .pipe(gulp.dest(DIST_TEST_PATH))
+    .pipe(livereload());
+});
+
+gulp.task(WATCH_TASK, WATCH_TASKS, () => {
+  livereload.listen();
+  connect.server();
+  gulp.watch(WATCH_FILES, WATCH_TASKS);
+});
+
+gulp.task(CONNECT_TASK, () => {
+  connect.server();
+});
+
+gulp.task(DEFAULT_TASK, DEFAULT_TASKS);
--- tests/main.tests.js
+++
@@ -1,8 +0,0 @@
-var assert = require('assert')
-var xapije = require('lib/xAPI-javascript-events')
-
-describe('jsonizer library', function () {
-	it('should be defined', function () {
-		assert(xapije)
-	})
-})
